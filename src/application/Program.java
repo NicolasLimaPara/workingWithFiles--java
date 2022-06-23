e4256c7228c543316450e0307e8fe9a05c8d8eca
Nicolas Lima <c.nicolaslima@hotmail.com>
2022-06-23 19:13:47 -0300

    Informations about file path (functions)

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,25 +8,15 @@ public class Program {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner (System.in);
-		System.out.println("Enter a folder path: ");
+		
+		System.out.println("Enter a file path: ");
 		String strPath = sc.nextLine();
 		
-		File path = new File(strPath);
+		File path = new File (strPath);
 		
-		File[] folders = path.listFiles(File::isDirectory);
-		System.out.println("FOLDERS:");
-		for (File folder : folders) {
-			System.out.println(folder);
-		}
-		
-		File[] files = path.listFiles(File::isFile);
-		
-		for (File file : files) {
-			System.out.println(file);
-		}
-		
-		boolean success = new File(strPath + "\\folderstr").mkdir();
-		System.out.println("Directory successfully created: " +success );
+		System.out.println("getParent: " + path.getParent());
+		System.out.println("getName: " + path.getName());
+		System.out.println("getPath: " + path.getPath());
 		
 		sc.close();
 				
