85376296c0bcd6aff45b54a461965d97930e1ca4
Nicolas Lima <c.nicolaslima@hotmail.com>
2022-06-23 12:29:33 -0300

    Reading files with FileReader and BufferedReader

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,33 +1,45 @@
 package application;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-
+		String path = "c:\\temp\\testFiles.txt";
+		FileReader fr = null;
+		BufferedReader br = null;
 		
-		File file = new File ("c:\\temp\\testFiles.txt");
-		// aqui foi instanciado um objeto do tipo File. Esse objeto vai encapsular todo o precesso de acessar o arquivo e tbm o caminho desse arquivo.
-		Scanner sc =  null;
-		
-		// a abertura do arquivo deve ser feita dentro de um bloco try { pois na hora de abrir o arquivo pode ser gerada uma excecao do tipo IOExeption (Exception) --> precisa ser tratada.
-		try  {
-			sc = new Scanner(file);
-			while (sc.hasNextLine())
-				System.out.println(sc.nextLine());
+		try {
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+			 // o BufferedReader é instanciado a partir do FileReader
+			
+			String line = br.readLine();
+			
+			while(line!= null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
 		}
 		catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
 		}
 		finally {
-			if (sc!=null) { 
-				sc.close();
+			try {
+				if (br != null) {
+					br.close();
+					
+				}
+				if (fr != null) {
+					fr.close();
+				}
 			}
-			
+			catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 
 	}
