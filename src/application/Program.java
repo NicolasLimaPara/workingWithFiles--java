11858ff9bb35d0343aa929cbed9373f7fe249300
Nicolas Lima <c.nicolaslima@hotmail.com>
2022-06-23 12:14:57 -0300

    Introduction to File and Scanner to read a file

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,34 @@
 package application;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+
+		
+		File file = new File ("c:\\temp\\testFiles.txt");
+		// aqui foi instanciado um objeto do tipo File. Esse objeto vai encapsular todo o precesso de acessar o arquivo e tbm o caminho desse arquivo.
+		Scanner sc =  null;
+		
+		// a abertura do arquivo deve ser feita dentro de um bloco try { pois na hora de abrir o arquivo pode ser gerada uma excecao do tipo IOExeption (Exception) --> precisa ser tratada.
+		try  {
+			sc = new Scanner(file);
+			while (sc.hasNextLine())
+				System.out.println(sc.nextLine());
+		}
+		catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		finally {
+			if (sc!=null) { 
+				sc.close();
+			}
+			
+		}
 
 	}
 
